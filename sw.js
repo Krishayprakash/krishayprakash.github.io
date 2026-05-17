56e8c9eb9447f011d44568fb72042e315013249e
Krishay Prakash <prakashkrishay2012@gmail.com>
2026-05-17 18:51:09 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,26 @@
-const CACHE_NAME = "krishay-site-v2";
+const CACHE_NAME = "krishay-site";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./smallcraft.html",
-  "./logo.png",
-  "./manifest.json"
+  "/",
+  "/index.html",
+  "/smallcraft.html",
+  "/chess.html",
+  "/logo.png",
+  "/manifest.json"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
